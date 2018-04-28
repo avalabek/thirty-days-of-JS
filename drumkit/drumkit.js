f6f441be4f923efaa4b44611cdf0d4713257e0a1
Andreya <avalabek@yahoo.com>
2018-04-27 21:31:41 -0700

    put js in script tags in index.html and .remove works

--- drumkit/drumkit.js
+++ drumkit/drumkit.js
@@ -4,17 +4,18 @@ function playSound (e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return;//stops the funciton running
-  audio.currentTime =0;
+  audio.currentTime =0;//rewinds it so if you hit it again it repeats
   audio.play();
   key.classList.add('playing');
 }
 
 function removeTransition(e) {
+  console.log(e);
   if (e.propertyName !== 'transform') return;
   this.classList.remove('playing');
 }
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 window.addEventListener('keydown', playSound);
