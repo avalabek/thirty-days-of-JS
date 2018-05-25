bec8cd1c28b871c8645735e04929db6cd4599010
Andreya <avalabek@yahoo.com>
2018-05-25 10:43:43 -0700

    play pause scrub functioning

--- customVideo/scripts2.js
+++ customVideo/scripts2.js
@@ -1,19 +1,18 @@
-//get elements on page
+/* Get Our Elements */
 const player = document.querySelector('.player');
-const video = player.querySelector('viewer');
+const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
-//build functions
-function togglePlay(){
-  //nb paused is a property on the video already
+/* Build out functions */
+function togglePlay() {
   const method = video.paused ? 'play' : 'pause';
   video[method]();
 }
-  //i like the above way better just calling the method we've created on video
+//i like the above way better just calling the method we've created on video
 
   // way to do it with if/else statement
   // if (video.paused){
@@ -22,31 +21,30 @@ function togglePlay(){
   //   video.pause();
   // }
 
-function updateButton(){
+function updateButton() {
   const icon = this.paused ? '►' : '❚ ❚';
+  console.log(icon);
   toggle.textContent = icon;
-  
 }
-function skip(){
-  // console.log(this.dataset.skip);
-  video.currentTime += parseFloat(this.dataset.skip);
+
+function skip() {
+ video.currentTime += parseFloat(this.dataset.skip);
 }
-function handleRangeUpdate(){
+
+function handleRangeUpdate() {
   video[this.name] = this.value;
-  console.log(this.name);
-  console.log(this.value);
 }
-function handleProgress(){
-  const percent = (video.currentTime / video.duration) *100;
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
   progressBar.style.flexBasis = `${percent}%`;
 }
 
-function scrub (e){
+function scrub(e) {
   const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
   video.currentTime = scrubTime;
-  //scrubTime is getting a percentage by dividing by the offset widhth
-  console.log(e);
 }
+
 //hook up event listeners
 //instead of adding pause into method, we listen for when the video is paused and then call the function to update what the play/pause button looks like.
 video.addEventListener('click', togglePlay);
@@ -55,12 +53,12 @@ video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
 //will trigger when video is updateing its time code
 toggle.addEventListener('click', togglePlay);
-skipButtons.forEach(button => button.addEventListener('click',skip));
+skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
 let mousedown = false;
 progress.addEventListener('click', scrub);
-progress.addEventListener('mousemove', (e)=> mousedown && scrub(e));
-progress.addEventListener('mousedown', ()=> mousedown = true);
-progress.addEventListener('mouseup', ()=> mousedown = false);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
